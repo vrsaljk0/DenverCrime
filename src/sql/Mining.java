a705a9e0c4c06fceb3b7d28a0c10037700faa70b
Azra Subašić <44138037+hax91@users.noreply.github.com>
2019-05-10 12:00:57 +0200

    FilteredAssociator

---
+++ src/sql/Mining.java
@@ -0,0 +1,22 @@
+package denvercrime;
+
+//ZANEMARIMO WARNINGE 'illegal reflective access' AKO VAM SE JAVLJAJU JAVA NES SERE BZVZ (VERZIJA 8 i 9)
+
+import weka.associations.FilteredAssociator;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class Mining {
+	public static void main(String args[]) throws Exception {
+		//uèitamo dataset
+		String dataset = "C:\\Users\\Aspire\\Desktop\\FAKS\\Programsko inž-2019\\baza\\crime_rules.csv";
+		DataSource source = new DataSource(dataset);
+		Instances data = source.getDataSet();
+		
+		FilteredAssociator model = new FilteredAssociator();
+		//build the associator on the filtered data -->za one koji žele znati više: http://weka.sourceforge.net/doc.stable/
+		model.buildAssociations(data);
+		//ispišemo rulove
+		System.out.println(model);
+	}
+}

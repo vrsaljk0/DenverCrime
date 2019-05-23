8d08383ab76435993138c5dfe0296416440a8745
Azra Subašić <44138037+hax91@users.noreply.github.com>
2019-05-23 14:02:16 +0200

    added settings for better rules

--- src/sql/Mining.java
+++ src/sql/Mining.java
@@ -1,21 +1,28 @@
-package sql;
+package denvercrime;
 
 //ZANEMARIMO WARNINGE 'illegal reflective access' AKO VAM SE JAVLJAJU JAVA NES SERE BZVZ (VERZIJA 8 i 9)
 
-import weka.associations.FilteredAssociator;
+import weka.associations.Apriori;
 import weka.core.Instances;
+import weka.core.SelectedTag;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class Mining {
 	
 	public static void getRules() throws Exception {
 		//uèitamo dataset koji smo dobili u Create_CSV !!!
-		String dataset = "C:\\Users\\Aspire\\Desktop\\FAKS\\Programsko inž-2019\\baza\\crime_rules.csv"; //prilagoditi path
+		String dataset = "C:\\Users\\Aspire\\Desktop\\FAKS\\Programsko inž-2019\\baza\\crime_sreden.csv"; //prilagoditi path
 		System.out.println(dataset);
 		DataSource source = new DataSource(dataset);
 		Instances data = source.getDataSet();
+				
+		Apriori model = new Apriori();
 		
-		FilteredAssociator model = new FilteredAssociator();
+		model.setLowerBoundMinSupport(0.001);
+		SelectedTag t = new SelectedTag(3, Apriori.TAGS_SELECTION);
+		model.setMetricType(t);
+		model.setNumRules(500);
+		model.setUpperBoundMinSupport(2.0);
 		//build the associator on the filtered data -->za one koji žele znati više: http://weka.sourceforge.net/doc.stable/
 		model.buildAssociations(data);
 		//ispišemo rulove

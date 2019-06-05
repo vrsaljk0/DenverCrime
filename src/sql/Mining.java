7e92f9ecbe0cb5c94cd8c991b295f26aac7f752c
Azra Subašić <44138037+hax91@users.noreply.github.com>
2019-06-05 21:11:40 +0200

    added FPGrowth

--- src/sql/Mining.java
+++ src/sql/Mining.java
@@ -1,33 +1,55 @@
 package sql;
 
-//ZANEMARIMO WARNINGE 'illegal reflective access' AKO VAM SE JAVLJAJU JAVA NES SERE BZVZ (VERZIJA 8 i 9)
-
-import weka.associations.Apriori;
+import weka.associations.*;
 import weka.core.Instances;
 import weka.core.SelectedTag;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NominalToBinary;
+import weka.filters.unsupervised.attribute.NumericToBinary;
 
 public class Mining {
 	
 	public static String getRules() throws Exception {
-		//uèitamo dataset koji smo dobili u Create_CSV !!!
-		//String dataset = "C:\\Users\\Aspire\\Desktop\\FAKS\\Programsko inž-2019\\baza\\crime_sreden.csv"; //prilagoditi path, zakomentirajte svoj da ne mijenjamo stalno
 		//String dataset = "C:\\Users\\Lukaku\\Documents\\programsko\\crime_sreden.csv";
-		String dataset = "C:\\Romano\\2.god\\Objektno\\eclipse_vj\\MySql-vjezba\\crime_sreden.csv";
-		System.out.println(dataset);
+		String dataset = "C:\\Romano\\2.god\\Objektno\\eclipse_vj\\MySql-vjezba\\crime.csv";
 		DataSource source = new DataSource(dataset);
 		Instances data = source.getDataSet();
 				
 		Apriori model = new Apriori();
-		
 		model.setLowerBoundMinSupport(0.001);
 		SelectedTag t = new SelectedTag(3, Apriori.TAGS_SELECTION);
 		model.setMetricType(t);
 		model.setNumRules(500);
 		model.setUpperBoundMinSupport(2.0);
-		//build the associator on the filtered data -->za one koji žele znati više: http://weka.sourceforge.net/doc.stable/
 		model.buildAssociations(data);
-		//ispišemo rulove
+		System.out.println(model);
+		
+		return model.toString();
+	}
+	
+	public static String getRulesFP() throws Exception {
+		//String dataset = "C:\\Users\\Lukaku\\Documents\\programsko\\crime_sreden.csv";
+		String dataset = "C:\\Romano\\2.god\\Objektno\\eclipse_vj\\MySql-vjezba\\crime.csv";
+		DataSource source = new DataSource(dataset);
+		Instances data = source.getDataSet();
+		
+		NominalToBinary nominalToBinary = new NominalToBinary();
+		nominalToBinary.setInputFormat(data);
+		data = Filter.useFilter(data, nominalToBinary);
+		
+		NumericToBinary numericToBinary = new NumericToBinary();
+		numericToBinary.setInputFormat(data);
+		data = Filter.useFilter(data, numericToBinary);
+				
+		FPGrowth model = new FPGrowth();
+		model.setLowerBoundMinSupport(0.001);
+		SelectedTag t = new SelectedTag(3, Apriori.TAGS_SELECTION);
+		model.setMetricType(t);
+		model.setUpperBoundMinSupport(2.0);
+		model.setLowerBoundMinSupport(2.0);
+		model.setNumRulesToFind(500);
+		model.buildAssociations(data);
 		System.out.println(model);
 		
 		return model.toString();
